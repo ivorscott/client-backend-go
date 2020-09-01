91cf22dd072f148ba0cbab5b414aff27da9c842b
Ivor Scott <ivor@devpie.io>
2020-09-01 21:32:35 +0200

    refactor: remove client application

--- client/src/App/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const button = getByText(/Sign Up/i);
-  expect(button).toBeInTheDocument();
-});
--- client/src/App/Products/types.ts
+++
@@ -1,6 +0,0 @@
-export interface IProduct {
-  id: string;
-  name: string;
-  description: string;
-  price: number;
-}
