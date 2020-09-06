9a95388b679759ab79802e1e1c129777cbfa4649
Ivor Scott <ivor@devpie.io>
2020-09-06 10:16:25 +0200

    feat: add default roles array and combine default and assigned roles

--- settings/tenant/rules/set-claim--assigned-role.js
+++ settings/tenant/rules/set-claim--assigned-role.js
@@ -1,13 +1,13 @@
 function SetAssignedRoles(user, context, callback) {
     const namespace = 'https://client.devpie.io/claims/roles';
-    const defaultRole = context.idToken[namespace];
-    const assignedRoles = (context.authorization || {}).roles;
+    const defaultRoles = context.idToken[namespace];
+    const assignedRoles = (context.authorization || {}).roles.push(defaultRoles)
 
     let idTokenClaims = context.idToken || {};
     let accessTokenClaims = context.accessToken || {};
 
-    idTokenClaims[namespace] = assignedRoles || defaultRole;
-    accessTokenClaims[namespace] = assignedRoles || defaultRole;
+    idTokenClaims[namespace] = assignedRoles;
+    accessTokenClaims[namespace] = assignedRoles;
 
     context.idToken = idTokenClaims;
     context.accessToken = accessTokenClaims;
--- settings/tenant/rules/set-claim--default-role.js
+++ settings/tenant/rules/set-claim--default-role.js
@@ -2,9 +2,9 @@ function SetDefaultRoles(user, context, callback) {
     const namespace = 'https://client.devpie.io/claims/roles';
 
     if (user.email.indexOf('@devpie.io') !== -1) {
-        context.idToken[namespace] = 'employee';
+        context.idToken[namespace] = ['employee'];
     } else {
-        context.idToken[namespace] = 'user';
+        context.idToken[namespace] = ['user'];
     }
 
     callback(null, user, context);
